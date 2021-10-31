78892f37684f0994efde583def8f0e2ffd1fc45a
Muhammad Zamroni <halo@matriphe.com>
2021-10-31 17:21:23 +0100

    Add comments to auth-client

--- pkg/repository/auth-client.go
+++ pkg/repository/auth-client.go
@@ -12,15 +12,18 @@ type (
 		ctx context.Context
 	}
 
+	// AuthClient is an interface for GitHub authenticated client.
 	AuthClient interface {
 		Client(token string) *github.Client
 	}
 )
 
+// NewAuthClient creates new GitHub authenticated client.
 func NewAuthClient(ctx context.Context) AuthClient {
 	return &authClient{ctx: ctx}
 }
 
+// Client returns GitHub authenticate client.
 func (a *authClient) Client(token string) *github.Client {
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
